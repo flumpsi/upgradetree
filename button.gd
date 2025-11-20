49c3ea0ae3d44c28bbb1129012c9df8a25539b92
flumpsi <flumpsi@outlook.com>
2025-11-20 14:01:20 +0100

    Added GPL 3.0, added artwork with gb style and also added large purchased label to show when objects are purchased

--- button.gd
+++ button.gd
@@ -1,3 +1,19 @@
+# Copyright (C) 2025 flumpsi
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 extends Button
 
 var firsttime
@@ -7,6 +23,7 @@ func _ready():
 	firsttime = true
 
 func _process(dt):
+	get_node("Purchased?").visible = not firsttime
 	if get_parent().reset == true:
 		var timer = get_node("Timer")
 		firsttime = true
--- upgrade_9.gd
+++ upgrade_9.gd
@@ -1,3 +1,18 @@
+# Copyright (C) 2025 flumpsi
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 extends Button
 
 var firsttime
@@ -8,6 +23,9 @@ func _ready():
 	firsttime = true
 	get_parent().get_node("Clicker").visible = false
 	get_parent().get_node("Upgrade10").visible = false
+
+func _process(dt):
+	get_node("Purchased?").visible = not firsttime
 
 func _on_pressed() -> void:
 	if firsttime == true and get_parent().sixSeven >= price:
